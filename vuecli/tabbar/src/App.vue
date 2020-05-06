ff2a758a2088336a29e7fd46b995daec01014422
BinAnt <binbuyanza@gmail.com>
2020-05-06 23:02:14 +0800

    tabbar封装

---
+++ vuecli/tabbar/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+  <TarBar></TarBar>
+</div>
+</template>
+
+<script>
+import TarBar from './components/tabbar/TabBar'
+export default {
+  name: 'App',
+  components: {
+    TarBar
+  }
+}
+</script>
+<style scoped>
+  @import './assets/css/base.css'
+</style>
---
+++ vuecli/tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="tab-bar">
+    <div class="tab-bar-item">首页</div>
+    <div class="tab-bar-item">分类</div>
+    <div class="tab-bar-item">购物车</div>
+    <div class="tab-bar-item">我的</div>
+  </div>
+</template>
+<script>
+    export default {
+        name: 'TarBar'
+    }
+</script>
+<style scoped>
+    #tab-bar {
+  width: 100%;
+  height: 90px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: #f1f1f1; 
+  margin: 0;
+  padding: 0;
+  }
+#tab-bar .tab-bar-item {
+  width: 25%;
+  text-align: center;
+}
+</style>
